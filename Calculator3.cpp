91a3604c8576bc7b48be062f028f5fbb8885a6f7
Tunjay Novruzzade - HLPL1 <79202527+TunjayNovruzzade@users.noreply.github.com>
2022-03-20 19:38:45 +0100

    Add files via upload

---
+++ Calculator3.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+
+void enterNumbers(int& a, int& b)
+
+{
+     cout << "Enter two numbers: ";
+     cin >> a >> b;
+}
+
+ 
+int sumNumbers(int a, int b, int& sum)
+
+{
+     sum = a + b;
+     cout << sum << endl;
+     return sum;
+}
+
+ 
+int divisionNumbers(int a, int b, int& sum)
+{
+     sum = a / b;
+     cout << sum << endl;
+     return sum;
+}
+
+
+int subtractionNumbers(int a, int b, int& sum)
+{
+     sum = a - b;
+     cout << sum << endl;
+      return sum;
+}
+
+
+int multiplyingNumbers(int a, int b, int& sum)
+{
+      sum = a * b;
+      cout << sum << endl;
+      return sum;
+}
+
+ 
+int main ()
+
+{
+     int a;
+     int b;
+     int Result;
+     
+     enterNumbers(a, b);
+     sumNumbers(a, b, Result);
+     divisionNumbers(a, b, Result);
+     subtractionNumbers(a, b, Result);
+     multiplyingNumbers(a, b, Result);
+  
+}
